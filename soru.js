613c1e620e1f8de1049536ec37859c8b5c3aaae7
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-10 12:22:46 +0300

    updte-questions

--- soru.js
+++ soru.js
@@ -9,10 +9,10 @@ Soru.prototype.cevabiKontrolEt = function(cevap){
 }
 
 let sorular = [
-    new Soru ("1-Hangisi javascript paket yönetim uygulamasıdır ?" , {a:"Node.js" , b:"Typescript" , c:"Npm" , d:"Nuget"} , "c"),
-    new Soru ("2-Hangisi javascript paket yönetim uygulamasıdır ?" , {a:"Node.js" , b:"Typescript" , c:"Npm" , d:"Nuget"} , "c"),
-    new Soru ("3-Hangisi javascript paket yönetim uygulamasıdır ?" , {a:"Node.js" , b:"Typescript" , c:"Npm" , d:"Nuget"} , "c"),
-    new Soru ("4-Hangisi javascript paket yönetim uygulamasıdır ?" , {a:"Node.js" , b:"Typescript" , c:"Npm" , d:"Nuget"} , "c"),
-    new Soru ("5-Hangisi javascript paket yönetim uygulamasıdır ?" , {a:"Node.js" , b:"Typescript" , c:"Npm" , d:"Nuget"} , "c")
+    new Soru ("1- Aşağıda Verilen İlk Çağ Uygarlıklarından Hangisi Yazıyı İcat Etmiştir ?" , {a:"Hititler "  ,b:"Elamlar " ,   c:"Sümerler "  ,d:"Urartular"} , "c"),
+    new Soru ("2-Mustafa Kemal Atatürk’ün Nüfusa Kayıtlı Olduğu İl Hangisidir ?" , {a:"Bursa " , b:"Ankara "  , c:"İstanbul "   ,d:"Gaziantep"} , "d"),
+    new Soru ("Tsunami Felaketinde En Fazla Zarar Gören Güney Asya Ülkesi Aşağıdakilerden Hangisidir ?" , {a:"Endonezya "  ,b:"Srilanka "  , c:"Tayland "   ,d:"Hindistan"} , "a"),
+    new Soru ("4-Romen Rakamında Hangi Sayı Yoktur ? " , {a:"0 " , b:"50 " ,  c:"100 "   ,d:"1000"} , "a"),
+    new Soru ("5-Aspirinin Hammaddesi Nedir ?" , {a:"Söğüt " , b:"Köknar ",   c:"Kavak "   ,d:"Meşe "} , "a")
 ];
 
